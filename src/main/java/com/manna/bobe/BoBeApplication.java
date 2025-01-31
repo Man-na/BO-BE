f9a0c46df32bb470d80a1c478adfef48b58ce966
홍종훈 <whdgnszz1@snu.ac.kr>
2025-01-31 14:04:40 +0900

    프로젝트 관리도구 BE 기본설정

---
+++ src/main/java/com/manna/bobe/BoBeApplication.java
@@ -0,0 +1,13 @@
+package com.manna.bobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manna/bobe/BoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manna.bobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
